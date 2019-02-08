2e2f0837912869234e049a82df30d0dffd3dfdb0
Dave Laird <kiwiupover@gmail.com>
2019-02-08 09:59:53 -0800

    broken in IE11

--- config/environment.js
+++ config/environment.js
@@ -16,11 +16,7 @@ module.exports = function(environment) {
         Date: false
       }
     },
-
-    phoneInput: {
-      lazyLoad: true
-    },
-
+    
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
